62568ff9cd5d97a371628e50d90751eb22c475a4
wangguoping <Kingdompin@163.com>
2017-11-11 00:38:05 +0800

    修改Generator

--- src/guo/ping/db2pojo/tools/Generator.java
+++ src/guo/ping/db2pojo/tools/Generator.java
@@ -1,7 +1,6 @@
 package guo.ping.db2pojo.tools;
 
 import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +10,7 @@ import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class Generator {
+
 	/**
 	 * 根据配置文件进行反向工程
 	 * @throws Exception
